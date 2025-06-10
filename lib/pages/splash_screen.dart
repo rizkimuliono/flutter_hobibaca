9a873242578fb2d3db79d0fe19baf5432cee0228
rizkimuliono <rizkimuliono@gmail.com>
2025-06-10 10:01:27 +0700

    memberi jeda waktu pada checkTokenValid() di splashscreen

--- lib/pages/splash_screen.dart
+++ lib/pages/splash_screen.dart
@@ -21,6 +21,7 @@ class _SplashScreenState extends State<SplashScreen> {
   }
 
   Future<void> checkTokenValid() async {
+    await Future.delayed(const Duration(seconds: 2)); //memberi jeda 1 detik
     final prefs = await SharedPreferences.getInstance();
     final token = prefs.getString('token');
 
